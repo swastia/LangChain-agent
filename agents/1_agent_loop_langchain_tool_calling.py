ae92ca8099efbc4f8176eb974e4bca7c21fd968e
swastia <swasti.arya.eb@gmail.com>
2026-04-17 08:12:33 +0530

    added system and user prompts, and iterations on the tools

--- agents/1_agent_loop_langchain_tool_calling.py
+++ agents/1_agent_loop_langchain_tool_calling.py
@@ -41,10 +41,46 @@ def apply_discount(price: float, discount_tier: str) -> float:
 
 
 # ----- Agent Loop -----
+# ---- Tool binding to llm objects is only supported by llms that support function calling ----
 @traceable(name="Langchain Agent Loop")
 def run_agent(question: str):
-    """Runs the agent loop to answer the question using the defined tools."""
-    pass
+    tools = [get_product_price, apply_discount]  # List of available tools for the agent
+    tools_dict = {tool.name: tool for tool in tools}  # Create a dictionary of tools for easy access
+    llm = init_chat_model(f'ollama:{MODEL}', temperature=0)  # Initialize the language model (e.g., LLaMA 3.2)
+    llm_with_tools = llm.bind_tools(tools)  # Wrap the language model with the tools
+    print(f'Question: {question}')
+    print("=" * 60)
+
+    # ---- Brain of the LLM -----
+    messages = [
+        SystemMessage(
+            content=(
+                "You are a helpful shopping assistant. "
+                "You have access to a product catalog tool and a discount application tool.\n\n"
+                "STRICT RULES - you must follow these exacty:\n"
+                "1. Never guess or assume any product price."
+                "You must ALWAYS call get_product_price to get the price of a product before applying any discount. "
+                "2. Only call apply_discount AFTER you have received "
+                "the price from get_product_price. Pass the exact price returned by get_product_price - do NOT pass a made-up number.\n"
+                "3. NEVER calculate discounts yourself using math. You MUST call the apply_discount tool to get the discounted price."
+                "4. If the user does not specify a discount tier, ask them which tier to use - do NOT assume one."
+                )
+        ),
+        HumanMessage(content=question)
+    ]
+
+    for iteration in range(MAX_ITERATIONS):
+        print(f"\n Iteration {iteration  + 1} of {MAX_ITERATIONS}")
+
+        ai_message = llm_with_tools.invoke(messages)  # Get the AI's response based on the current messages
+        tool_calls = ai_message.tool_calls  # Extract any tool calls made by the AI in its response
+
+        # If no tool calls were made, we assume the AI has provided a final answer and we can return it
+        if not tool_calls:
+            print("Final Answer:", ai_message.content)
+            return ai_message.content
+
+
 
 if __name__ == "__main__":
     # Example question for the agent to answer
@@ -52,3 +88,6 @@ if __name__ == "__main__":
     print(f"Question: {question}")
     result = run_agent(question)
     print(f"Final result: {result}")
+
+
+
